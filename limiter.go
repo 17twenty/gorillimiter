3e8e70bd6fb070ab897a26b0e66662f1d9b5fb89
Nick Glynn <nick.glynn@versent.com.au>
2020-05-07 17:04:22 +1000

    Add middleware compatible with mux chaining sig

--- limiter.go
+++ limiter.go
@@ -6,7 +6,40 @@ import (
 	"time"
 )
 
-// Limiter is the an LRU based middleware limiter for gorillmux
+// MiddlewareWrapper can be used for chained middleware
+func MiddlewareWrapper(requestsPerInterval int, interval time.Duration) func(http.Handler) http.Handler {
+
+	cache, err := NewLRU(1000, interval)
+	if err != nil {
+		log.Println("Couldn't create a cache - falling back to passthrough", err)
+		return func(next http.Handler) http.Handler {
+			return http.HandlerFunc(func(wr http.ResponseWriter, req *http.Request) {
+
+				next.ServeHTTP(wr, req)
+			})
+		}
+	}
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(wr http.ResponseWriter, req *http.Request) {
+			ip := getRemoteIP(req)
+
+			// Set a maximum of requestsPerInterval requests per interval
+			cnt, underRateLimit := cache.Inc(ip, requestsPerInterval)
+			if underRateLimit {
+				// we good son
+				next.ServeHTTP(wr, req)
+				return
+			}
+
+			log.Printf("Address [%s] is over ratelimit, denying for now, current hits [%d]\n", ip, cnt)
+			http.Error(wr, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		})
+	}
+}
+
+// Limiter is the an LRU based limiter for a gorilla mux
 // It's hardcoded to only remember most recent 1000 IP addresses
 // You choose how many requests are allowed per interval
 func Limiter(next http.Handler, requestsPerInterval int, interval time.Duration) http.Handler {
